5e459afec03011725bc99b5124fcc7493dfd5aa1
Tenkazito <alejogs2931@hotmail.com>
2025-05-13 20:50:09 -0500

    I forgot the cors port

--- Obito-back/src/app.js
+++ Obito-back/src/app.js
@@ -13,8 +13,8 @@ const app = express();
 app.use(morgan('dev')); // Logger para solicitudes HTTP
 app.use(express.json()); // Parsear JSON en las solicitudes
 app.use(cors({
-  //origin: process.env.FRONTEND_URL, // Permitir solicitudes desde el frontend
-  origin: "http://localhost:5173", // Permitir solicitudes desde el frontend
+  origin: process.env.FRONTEND_URL, // Permitir solicitudes desde el frontend
+  //origin: "http://localhost:5173", // Permitir solicitudes desde el frontend
 }));
 
 // Rutas
